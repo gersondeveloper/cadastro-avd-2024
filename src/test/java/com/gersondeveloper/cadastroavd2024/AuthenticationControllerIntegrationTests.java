723dcbfa86aabcb059131194069150e275396c11
Gerson C Filho <gersoncfilho@mac.com>
2024-04-16 07:51:50 +0100

    refact: refactored AuthenticationControllerIntegrationTests.java in order to use TestContainers

--- src/test/java/com/gersondeveloper/cadastroavd2024/AuthenticationControllerIntegrationTests.java
+++ src/test/java/com/gersondeveloper/cadastroavd2024/AuthenticationControllerIntegrationTests.java
@@ -1,8 +1,11 @@
 package com.gersondeveloper.cadastroavd2024;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gersondeveloper.cadastroavd2024.domain.dtos.request.UserAuthenticationRequestDto;
 import com.gersondeveloper.cadastroavd2024.domain.dtos.request.UserRegisterRequestDto;
 import com.gersondeveloper.cadastroavd2024.domain.entities.user.UserRole;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +13,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MSSQLServerContainer;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,6 +29,26 @@ public class AuthenticationControllerIntegrationTests {
     @Autowired
     private MockMvc mockMvc;
 
+    static MSSQLServerContainer mssqlserverContainer = new MSSQLServerContainer()
+            .acceptLicense();
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", mssqlserverContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mssqlserverContainer::getUsername);
+        registry.add("spring.datasource.password", mssqlserverContainer::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        mssqlserverContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll(){
+        mssqlserverContainer.stop();
+    }
+
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -30,7 +56,7 @@ public class AuthenticationControllerIntegrationTests {
     private PasswordEncoder passwordEncoder;
 
     @Test
-    public void testRegisterEndpoint() throws Exception {
+    public void testSuccessfulRegisterEndpoint() throws Exception {
         // Prepare request body
         UserRegisterRequestDto registerRequest = new UserRegisterRequestDto("teste@teste.com", "password", UserRole.ADMIN);
         String requestBody = objectMapper.writeValueAsString(registerRequest);
@@ -40,7 +66,57 @@ public class AuthenticationControllerIntegrationTests {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestBody))
                 .andExpect(MockMvcResultMatchers.status().isOk());
-        // Add more assertions as needed
+    }
+
+    @Test
+    public void testUnSuccessfulRegisterEndpoint() throws Exception {
+        // Prepare request body
+        UserRegisterRequestDto registerRequest = new UserRegisterRequestDto("teste@teste.com", "password", null);
+        String requestBody = objectMapper.writeValueAsString(registerRequest);
+
+        // Perform POST request to /register endpoint
+        mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    public void testLoginSuccessfulEndpoint() throws Exception{
+        // Prepare register user
+        UserRegisterRequestDto registerRequest = new UserRegisterRequestDto("teste@teste.com", "password", UserRole.ADMIN);
+        String requestBody = objectMapper.writeValueAsString(registerRequest);
+
+        // Prepare login
+        UserAuthenticationRequestDto userAuthenticationRequestDto = new UserAuthenticationRequestDto("teste@teste.com", "password");
+        String requestLoginBody = objectMapper.writeValueAsString(userAuthenticationRequestDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestLoginBody))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+    }
+
+    @Test
+    public void testLoginUnsuccessfulEndpoint() throws Exception{
+        // Prepare register user
+        UserRegisterRequestDto registerRequest = new UserRegisterRequestDto("teste@teste.com", "password2", UserRole.ADMIN);
+        String requestBody = objectMapper.writeValueAsString(registerRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        // Prepare login
+        UserAuthenticationRequestDto userAuthenticationRequestDto = new UserAuthenticationRequestDto("teste3434@teste.com", "password");
+        String requestLoginBody = objectMapper.writeValueAsString(userAuthenticationRequestDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestLoginBody))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+
     }
 
 }
