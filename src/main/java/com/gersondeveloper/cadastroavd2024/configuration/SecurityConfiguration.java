5c4b8e1e6e05600b232dd85f53c72edadbd51b4d
GERSON CARDOSO FILHO <gerson_cardoso@hotmail.com>
2025-08-10 18:46:00 +0100

    Update API endpoints with `/api` prefix and add CORS support for AuthenticationController.

--- src/main/java/com/gersondeveloper/cadastroavd2024/configuration/SecurityConfiguration.java
+++ src/main/java/com/gersondeveloper/cadastroavd2024/configuration/SecurityConfiguration.java
@@ -30,10 +30,10 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/api-docs").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/actuator").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/customers").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/customers").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
--- src/main/java/com/gersondeveloper/cadastroavd2024/controllers/AuthenticationController.java
+++ src/main/java/com/gersondeveloper/cadastroavd2024/controllers/AuthenticationController.java
@@ -18,17 +18,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("api/auth")
+@CrossOrigin(value = "http://localhost:4200")
 public class AuthenticationController {
 
     @Autowired
