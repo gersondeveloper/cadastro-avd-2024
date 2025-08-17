68a200a5db6a390b39c7aacde8dd6af1a5dabbfe
GERSON CARDOSO FILHO <gerson_cardoso@hotmail.com>
2025-08-17 17:52:31 +0100

    refact CustomerResponseDto.java

--- src/main/java/com/gersondeveloper/cadastroavd2024/domain/dtos/response/CustomerResponseDto.java
+++ src/main/java/com/gersondeveloper/cadastroavd2024/domain/dtos/response/CustomerResponseDto.java
@@ -4,5 +4,5 @@ import com.gersondeveloper.cadastroavd2024.domain.entities.enums.UserRole;
 
 import java.time.LocalDateTime;
 
-public record CustomerResponseDto(long id, String name, String email, String contactName, String phone,  UserRole role) {
+public record CustomerResponseDto(long id, String name, String email, String contactName, String phone, UserRole role, LocalDateTime creationDate, boolean isActive) {
 }
