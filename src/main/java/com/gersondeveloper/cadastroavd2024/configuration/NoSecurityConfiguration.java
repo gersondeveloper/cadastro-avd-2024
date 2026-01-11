f414a2c0922ce2838225fd7f7174033d357fff77
GERSON CARDOSO FILHO <gerson_cardoso@hotmail.com>
2026-01-11 08:23:00 +0000

    refactor: enhance NoSecurityConfiguration to support CORS with specific origins

--- src/main/java/com/gersondeveloper/cadastroavd2024/configuration/NoSecurityConfiguration.java
+++ src/main/java/com/gersondeveloper/cadastroavd2024/configuration/NoSecurityConfiguration.java
@@ -1,20 +1,41 @@
 package com.gersondeveloper.cadastroavd2024.configuration;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @Profile({"dev", "test", "local"})
 public class NoSecurityConfiguration {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        http
+                .cors(Customizer.withDefaults()) // Habilita o CORS no Spring Security
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedOrigins(List.of("http://localhost:4200")); // Origem do seu Angular
+        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        cors.setAllowedHeaders(List.of("*"));
+        cors.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+        return source;
     }
 }
