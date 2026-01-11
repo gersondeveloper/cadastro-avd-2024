63ec2e8c62d281c45dacbc15fdc5a101264cee56
GERSON CARDOSO FILHO <gerson_cardoso@hotmail.com>
2026-01-11 11:11:41 +0000

    feature: creating category endpoint

---
+++ src/main/java/com/gersondeveloper/cadastroavd2024/domain/dtos/request/CreateCategoryRequest.java
@@ -0,0 +1,4 @@
+package com.gersondeveloper.cadastroavd2024.domain.dtos.request;
+
+public record CreateCategoryRequest() {
+}
---
+++ src/main/java/com/gersondeveloper/cadastroavd2024/infra/services/CategoryService.java
@@ -0,0 +1,7 @@
+package com.gersondeveloper.cadastroavd2024.infra.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+}
